4018dfc4a0072c91232a2c78770e9a0c6ec2e53f
Combii <davidstovle@gmail.com>
2017-03-14 10:49:32 +0100

    Moved to Logic

--- src/main/java/Logic/Ticket.java
+++ src/main/java/Logic/Ticket.java
@@ -1,8 +1,49 @@
 package Logic;
 
+import Logic.Movie;
+import Logic.Seat;
+import Logic.Theater;
+
+import java.util.Date;
+
 /**
- * Created by David Stovlbaek
- * 14 March 2017.
+ * Created by BorisGrunwald on 14/03/2017.
  */
 public class Ticket {
+
+    private Seat seat;
+    private Theater t;
+    private Movie movie;
+    private Date d;
+
+    public Ticket(Seat seat, Theater t, Movie movie) {
+        this.seat = seat;
+        this.t = t;
+        this.movie = movie;
+        this.d = new Date();
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public Theater getT() {
+        return t;
+    }
+
+    public void setT(Theater t) {
+        this.t = t;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
 }

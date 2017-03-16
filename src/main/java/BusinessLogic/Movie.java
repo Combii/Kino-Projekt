98e194e7f04757d9aea6f3135c774035a22b80c9
Combii <davidstovle@gmail.com>
2017-03-16 13:28:22 +0100

    Added

--- src/main/java/BusinessLogic/Movie.java
+++ src/main/java/BusinessLogic/Movie.java
@@ -8,5 +8,12 @@ public class Movie {
     private String name;
     private int ageRestriction;
     private double price;
+    private String genre;
 
+    public Movie(String name, int ageRestriction, double price, String genre) {
+        this.name = name;
+        this.ageRestriction = ageRestriction;
+        this.price = price;
+        this.genre = genre;
+    }
 }

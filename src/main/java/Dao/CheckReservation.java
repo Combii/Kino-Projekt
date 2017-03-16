cac7da9f90505dd90d9c054f323a543ba7ba50b9
Combii <davidstovle@gmail.com>
2017-03-16 11:37:53 +0100

    Added Class

---
+++ src/main/java/Dao/CheckReservation.java
@@ -0,0 +1,31 @@
+package Dao;
+
+import BusinessLogic.Seat;
+
+import java.sql.SQLException;
+
+/**
+ * Created by David Stovlbaek
+ * 16 March 2017.
+ */
+public class CheckReservation {
+
+
+
+    public static boolean checkSeatIsValid(Seat seat){
+        try {
+            SQLDatabase database = SQLDatabase.getDatabase();
+
+
+            int seatNumber = seat.getNumber();
+            seat.get
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}

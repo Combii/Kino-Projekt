f5b5b5c13609a120a68e7242bae62b861a9761ce
Combii <davidstovle@gmail.com>
2017-03-16 13:08:56 +0100

    Added new test

--- src/test/java/Dao/CheckReservationTest.java
+++ src/test/java/Dao/CheckReservationTest.java
@@ -12,6 +12,10 @@ public class CheckReservationTest extends TestCase {
         Seat seat = new Seat(1, 1);
         System.out.println(CheckReservation.checkSeatIsValid(seat));
         assertEquals(CheckReservation.checkSeatIsValid(seat), true);
+
+        Seat seat2 = new Seat(2, 2);
+        System.out.println(CheckReservation.checkSeatIsValid(seat2));
+        assertEquals(CheckReservation.checkSeatIsValid(seat2), false);
     }
 
 }

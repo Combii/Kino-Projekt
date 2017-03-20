71581c863214b594d0cbb7207290c3cd3e79cd85
Julius <gudenjulius@gmail.com>
2017-03-20 13:15:37 +0100

    wtf

--- src/main/java/BusinessLogic/Seat.java
+++ src/main/java/BusinessLogic/Seat.java
@@ -1,10 +1,5 @@
 package BusinessLogic;
 
-import com.sun.javafx.scene.control.skin.VirtualFlow;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by BorisGrunwald on 14/03/2017.
  */
@@ -13,15 +8,10 @@ public class Seat {
     private int number;
     private int row;
     private boolean isReserved;
-    private int theater; //Which theater seat is in. 1 Big 2 small
 
-    //private List<Seat> list = new ArrayList<Seat>();
-    //List<Seat> bigTheater = new ArrayList<Seat>();
-
-    public Seat(int row, int number, boolean isReserved, int theater) {
+    public Seat(int row, int number, boolean isReserved) {
         this.number = number;
         this.row = row;
-        this.theater = theater;
         this.isReserved = isReserved;
     }
 
@@ -31,19 +21,5 @@ public class Seat {
 
     public int getRow() {
         return row;
-    }
-
-    public int getTheater() {
-        return theater;
-    }
-
-    @Override
-    public String toString() {
-        return "Seat{" +
-                "number=" + number +
-                ", row=" + row +
-                ", isReserved=" + isReserved +
-                ", theater=" + theater +
-                '}';
     }
 }

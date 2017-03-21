9fbf8826654d59400767b42bac467144d33041b2
Combii <davidstovle@gmail.com>
2017-03-21 19:47:30 +0100

    Added two classes in DAO

---
+++ src/main/java/Dao/SQLPicture.java
@@ -0,0 +1,54 @@
+package Dao;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by David Stovlbaek
+ * 21 March 2017.
+ */
+public class SQLPicture {
+
+
+    public void uploadPictureToDB(File file) throws SQLException, FileNotFoundException {
+        SQLDatabase database = SQLDatabase.getDatabase();
+        Connection conn = database.getConnection();
+
+        String sql = "INSERT INTO Picture (picture) values (?)";
+        PreparedStatement statement = conn.prepareStatement(sql);
+        InputStream inputStream = new FileInputStream(file);
+        statement.setBlob(1, inputStream);
+
+        statement.executeUpdate();
+
+    }
+
+
+    public File getPicture(String movieName) throws SQLException, FileNotFoundException {
+        /*
+        SQLDatabase database = SQLDatabase.getDatabase();
+
+        ResultSet resultSet = database.query("SELECT picture from Picture where pictureID = 1");
+
+        InputStream binaryStream = resultSet.getBinaryStream(1);
+
+        OutputStream outputStream = new FileOutputStream(new File("/Users/mkyong/Downloads/holder-new.js"));
+
+        int read = 0;
+        byte[] bytes = new byte[1024];
+
+        while ((read = inputStream.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, read);
+        }
+        */
+        return null;
+
+    }
+
+    private boolean checkIfFileExist(String path){
+    return false;
+    }
+}
---
+++ src/test/java/Dao/SQLPictureTest.java
@@ -0,0 +1,29 @@
+package Dao;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by David Stovlbaek
+ * 21 March 2017.
+ */
+public class SQLPictureTest {
+    @Test
+    public void uploadPictureToDB() throws Exception {
+        SQLPicture sqlPicture = new SQLPicture();
+
+        sqlPicture.uploadPictureToDB(new File("/Users/Combii/Dropbox/Datamatiker/3. Semester/Agil Aflevering/Kino Aflevering/src/main/Resources/MoviePictures/theGrey.jpg"));
+    }
+
+    @Test
+    public void getPicture() throws SQLException {
+        SQLPicture sqlPicture = new SQLPicture();
+
+        sqlPicture.getPicture("");
+    }
+
+}

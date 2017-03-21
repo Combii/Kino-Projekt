d010dd508ff5527ea11b56b6bfa7820a843adafa
jikol1906 <borisgrunwald@gmail.com>
2017-03-21 11:51:28 +0100

    can now reserve and unreserve seats

--- src/main/java/Dao/SQLreservation.java
+++ src/main/java/Dao/SQLreservation.java
@@ -12,25 +12,48 @@ import java.util.List;
 public class SQLreservation {
 
     SQLDatabase database = SQLDatabase.getDatabase();
-    Connection conn = database.getConnection();
-    PreparedStatement ps;
 
     public SQLreservation() throws SQLException {
     }
 
+    //-------------------------GET RESERVATIONS--------------------------------//
+
+
     public List<Seat> getSeatsForMovie(String movieName, Timestamp dateAndTime, int theaterType) throws SQLException {
 
-        ResultSet movieScheduleIdAndTheaterType = getMovieScheduleId(movieName,dateAndTime, theaterType);
-        movieScheduleIdAndTheaterType.next();
-        int movieScheduleId = movieScheduleIdAndTheaterType.getInt("movieScheduleID");
+        int movieScheduleId = getMovieScheduleId(movieName,dateAndTime,theaterType);
         return getSeatList(movieScheduleId);
+
+    }
+
+    //-------------------------ADD RESERVATIONS--------------------------------//
+
+    public void reserveSeats(String movieName, Timestamp dateAndTime, int theaterType, List<Seat> seatsToReserve) throws SQLException {
+
+        setIsReserved(true,seatsToReserve,movieName,dateAndTime,theaterType);
+    }
+
+    public void removeReservations(String movieName, Timestamp dateAndTime, int theaterType, List<Seat> seatsToReserve) throws SQLException {
+
+        setIsReserved(false,seatsToReserve,movieName,dateAndTime,theaterType);
+
+    }
+
+    //----------------------------PRIVATE METHODS-------------------------------//
+
+    private void setIsReserved(boolean reserve, List<Seat> seats, String movieName, Timestamp dateAndTime, int theaterType) throws SQLException {
+
+        int movieScheduleId = getMovieScheduleId(movieName,dateAndTime,theaterType);
+
+        for(Seat toReserve: seats) {
+            database.queryUpdate("UPDATE Seat SET isReserved = "+reserve+" WHERE movieScheduleID = "+movieScheduleId+" AND seatNumber = "+toReserve.getNumber()+" AND seatRow = "+toReserve.getRow()+";");
+        }
 
     }
 
     private List<Seat> getSeatList(int movieScheduleId) throws SQLException {
 
-        ps = conn.prepareStatement("SELECT seatRow,seatNumber,isReserved FROM Seat WHERE movieScheduleID = '"+movieScheduleId+"';");
-        ResultSet seat = ps.executeQuery();
+        ResultSet seat = database.query("SELECT seatRow,seatNumber,isReserved FROM Seat WHERE movieScheduleID = '"+movieScheduleId+"';");
 
         List<Seat> seats = new LinkedList<>();
 
@@ -46,17 +69,19 @@ public class SQLreservation {
         return seats;
     }
 
-    private ResultSet getMovieScheduleId(String movieName, Timestamp dateAndTime, int theaterType) {
+    private int getMovieScheduleId(String movieName, Timestamp dateAndTime, int theaterType) {
 
         try {
-            ps = conn.prepareStatement("SELECT movieScheduleID FROM MovieSchedule WHERE movieName = '"+movieName+"' AND movieDate = '"+dateAndTime+"' AND theaterType = '" + theaterType +"';");
-            return ps.executeQuery();
-
+            ResultSet movieScheduleID = database.query("SELECT movieScheduleID FROM MovieSchedule WHERE movieName = '"+movieName+"' AND movieDate = '"+dateAndTime+"' AND theaterType = '" + theaterType +"';");
+            movieScheduleID.next();
+            return movieScheduleID.getInt("movieScheduleID");
         } catch (SQLException e) {
-            return null;
+            return -1;
         }
 
     }
+
+
 
 
 }
---
+++ src/test/java/Dao/SQLreservationTest.java
@@ -0,0 +1,73 @@
+package Dao;
+
+import BusinessLogic.Seat;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by BorisGrunwald on 21/03/2017.
+ */
+public class SQLreservationTest {
+
+    SQLreservation res;
+
+    @Before
+    public void setUp() throws SQLException {
+        res = new SQLreservation();
+    }
+
+
+    @Test
+    public void testReserveSeats() throws SQLException {
+
+        List<Seat> toReserve = new ArrayList<>();
+
+        Seat s1 = new Seat(1,5, false);
+        Seat s2 = new Seat(1,6, false);
+
+        toReserve.add(s1);
+        toReserve.add(s2);
+
+        String moviename = "Shrek";
+        int theaterType = 2;
+        Timestamp timeAndDate = Timestamp.valueOf("2017-03-17 19:00:00");
+
+        res.reserveSeats(moviename,timeAndDate,theaterType,toReserve);
+
+
+
+
+
+    }
+
+    @Test
+    public void testRemoveReservation() throws SQLException {
+
+        List<Seat> toRemove = new ArrayList<>();
+
+        Seat s1 = new Seat(1,5, false);
+        Seat s2 = new Seat(1,6, false);
+
+        toRemove.add(s1);
+        toRemove.add(s2);
+
+        String moviename = "Shrek";
+        int theaterType = 2;
+        Timestamp timeAndDate = Timestamp.valueOf("2017-03-17 19:00:00");
+
+        res.removeReservations(moviename,timeAndDate,theaterType,toRemove);
+
+
+
+
+
+    }
+
+}

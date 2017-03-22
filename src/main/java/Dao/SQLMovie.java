8d8b55b4a84a5c079093a84e07271235ffd7432a
Combii <davidstovle@gmail.com>
2017-03-22 15:54:36 +0100

    RefactoringMovie (#49)
    * Better Code
    
    * Batman picture

--- src/main/java/Dao/SQLMovie.java
+++ src/main/java/Dao/SQLMovie.java
@@ -1,9 +1,9 @@
 package Dao;
 
 import BusinessLogic.Movie.Movie;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -56,7 +56,7 @@ public class SQLMovie {
             picturePath.next();
             check = picturePath.getInt(1);
             if(check == 0){
-            SQLPicture.uploadPictureToDB(new File(toAdd.getPicturePath()));
+            uploadPictureToDB(new File(toAdd.getPicturePath()));
             }
 
             PreparedStatement ps = conn.prepareStatement("INSERT INTO Movie VALUES ('" + toAdd.getMovieName() + "','" + toAdd.getGenre() + "','" + toAdd.getAgeRestriction() + "','" + toAdd.getPrice() + "','" + toAdd.getPictureFileName() + "');");
@@ -67,6 +67,73 @@ public class SQLMovie {
         }
 
     }
+
+    static void uploadPictureToDB(File file) throws SQLException, FileNotFoundException {
+        SQLDatabase database = SQLDatabase.getDatabase();
+        Connection conn = database.getConnection();
+
+        String sql = "INSERT INTO Picture (pictureName, picture) values (?, ?)";
+        PreparedStatement statement = conn.prepareStatement(sql);
+
+        statement.setString(1, file.getName());
+
+
+        InputStream inputStream = new FileInputStream(file);
+        statement.setBlob(2, inputStream);
+
+        statement.executeUpdate();
+    }
+
+    public List<Movie> getAllMovies() throws SQLException {
+
+            List<Movie> movies = new ArrayList<>();
+
+            ps = conn.prepareStatement("SELECT * FROM Movie;");
+            ResultSet movieRs = ps.executeQuery();
+
+            while(movieRs.next()) {
+                movies.add(getMovie(movieRs));
+            }
+
+            //Needed if missing picture locally
+            for (Movie movie : movies){
+                getPicture(movie.getPictureFileName());
+            }
+
+            return movies;
+    }
+
+    private Movie getMovie(ResultSet movie) throws SQLException {
+        return new Movie(movie.getString(1),movie.getString(2),movie.getString(3),movie.getDouble(4), movie.getString(5));
+    }
+
+    File getPicture(String movieNameFile) {
+        try {
+            SQLDatabase database = SQLDatabase.getDatabase();
+            ResultSet resultSet = database.query("SELECT * from Picture where pictureName = '"+ movieNameFile +"'");
+            resultSet.next();
+            String fileName = new File("src/main/Resources/MoviePictures").getAbsolutePath() + "/" + resultSet.getString(1);
+
+            File file = new File(fileName);
+
+
+            if(!file.exists()) {
+                InputStream inputStream = resultSet.getBinaryStream(2);
+                OutputStream outputStream = new FileOutputStream(file);
+
+                //Using library to extract from input to output
+                IOUtils.copy(inputStream, outputStream);
+            }
+
+            return file;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 
     public void deleteMovie(String name) throws SQLException {
 
@@ -86,29 +153,6 @@ public class SQLMovie {
             return null;
         }
 
-    }
-
-    public List<Movie> getAllMovies() throws SQLException {
-
-            List<Movie> movies = new ArrayList<>();
-
-            ps = conn.prepareStatement("SELECT * FROM Movie;");
-            ResultSet movieRs = ps.executeQuery();
-
-            while(movieRs.next()) {
-                movies.add(getMovie(movieRs));
-            }
-
-            //Needed if missing picture locally
-            for (Movie movie : movies){
-                SQLPicture.getPicture(movie.getPictureFileName());
-            }
-
-            return movies;
-    }
-
-    private Movie getMovie(ResultSet movie) throws SQLException {
-        return new Movie(movie.getString(1),movie.getString(2),movie.getString(3),movie.getDouble(4), movie.getString(5));
     }
 
 }
